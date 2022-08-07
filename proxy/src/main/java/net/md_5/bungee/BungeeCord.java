18df0a67e8e834c90c5df264e8ffeb70ac4847b9
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2022-08-07 13:00:52 +0200

    Update BungeeCord.java
    remove check since it is in the abstract declaration

--- proxy/src/main/java/net/md_5/bungee/BungeeCord.java
+++ proxy/src/main/java/net/md_5/bungee/BungeeCord.java
@@ -679,13 +679,13 @@ public class BungeeCord extends ProxyServer
     }
 
     @Override
-    public ServerInfo constructServerInfo(String name, InetSocketAddress address, String motd, boolean restricted, boolean ipForward = false)
+    public ServerInfo constructServerInfo(String name, InetSocketAddress address, String motd, boolean restricted, boolean ipForward)
     {
         return constructServerInfo( name, (SocketAddress) address, motd, restricted, ipForward );
     }
 
     @Override
-    public ServerInfo constructServerInfo(String name, SocketAddress address, String motd, boolean restricted, boolean ipForward = false)
+    public ServerInfo constructServerInfo(String name, SocketAddress address, String motd, boolean restricted, boolean ipForward)
     {
         return new BungeeServerInfo( name, address, motd, restricted, ipForward );
     }
